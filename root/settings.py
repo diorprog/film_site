8f7d196995315b4d622918c6df85a741436adff4
dior_prog <diyorbekdilmurodov1@gmail.com>
2023-11-11 11:25:15 +0500

    Site

--- root/settings.py
+++ root/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'root.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'film_db',
-        'USER': 'postgres',
-        'PASSWORD': '1',
-        'HOST': 'localhost',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
